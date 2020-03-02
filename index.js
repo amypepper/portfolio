1285861f9a5e24e354ed5fb6e2301a058a00e2aa
Amy Pepper <amyandmicahpepper@gmail.com>
2020-03-02 07:28:31 -0800

    more separating html and optimizing mobile css

--- index.js
+++ index.js
@@ -1,58 +1,5 @@
 "use strict";
 
-const quizApp = `<section class="text quiz-app-text">
-        <img
-          class="screenshot hero"
-          src="Images/Compressed/Quiz-app-1.png"
-          alt="screenshot of a quiz about the languages of the world"
-        />
-        <p class="quiz-app-text"><em>Title:</em> Languages of the World Quiz</p>
-        <p class="quiz-app-text">
-          <em>Description:</em> This is a 6-question multiple-choice quiz about
-          human languages from around the world. Users choose from 4 answer
-          choices, move through the questions one at a time, and then receive
-          immediate feedback about their answer. They're presented with a final
-          score, a link to the Ethnologue website to learn more, and an
-          opportunity to retake the quiz.
-        </p>
-        <p class="quiz-app-text">
-          <em>Technologies:</em> HTML5, CSS3, Javascript, and jQuery.
-        </p>
-
-        
-          <a class="link-button" href="https://amypepper.github.io/quiz-app/" target="_blank"
-            >Live App</a>
-       
-          <a class="link-button" href="https://github.com/amypepper/quiz-app" target="_blank"
-            >Repo</a>
-      
-        <p class="link image-link">See Full-Size Images</p>
-        <div class="screenshot-thumbs-flex">
-          <img
-            class="screenshot-thumbs"
-            src="Images/Compressed/Thumbs/Quiz-app-thumb-correct.png"
-            alt="thumbnail of correct answer display"
-          />
-          <img
-            class="screenshot-thumbs"
-            src="Images/Compressed/Thumbs/Quiz-app-thumb-user-error.png"
-            alt="thumbnail of user error notification"
-          />
-        </div>
-        <div class="screenshot-thumbs-flex">
-          <img
-            class="screenshot-thumbs"
-            src="Images/Compressed/Thumbs/Quiz-app-thumb-incorrect.png"
-            alt="thumbnail of incorrect answer display"
-          />
-          <img
-            class="screenshot-thumbs"
-            src="Images/Compressed/Thumbs/Quiz-app-thumb-final.png"
-            alt="thumbnail of final score display"
-          />
-        </div>
-      </section>`;
-
 const quizAppScreenshots = `<div
         class="screenshots-full-flex"
         role="figure"
@@ -81,13 +28,13 @@ const quizAppScreenshots = `<div
 
 // ******************RENDER FUNCTIONS**********************
 
-function renderLanding() {
-  $(".js-main").html(landing);
-}
+// function renderLanding() {
+//   $(".js-main").html(landing);
+// }
 
-function renderAbout() {
-  $(".js-main").html(about);
-}
+// function renderAbout() {
+//   $(".js-main").html(about);
+// }
 
 // function () {
 //   $(".js-main").html(projectLanding);
@@ -100,9 +47,9 @@ function renderAbout() {
 //   $(".js-main").html(quizApp);
 // }
 
-function renderFullScreenshots() {
-  $(".js-main").html(quizAppScreenshots);
-}
+// function renderFullScreenshots() {
+//   $(".js-main").html(quizAppScreenshots);
+// }
 
 // ******************************EVENT LISTENERS********************
 
@@ -158,27 +105,27 @@ function renderFullScreenshots() {
 //     renderFullScreenshots();
 //   });
 // }
-function callToActionLink() {
-  $(".js-main").on("click", ".js-call-to-action", event => {
-    event.preventDefault();
-    $(".js-projects-link").addClass("menu-focus");
-    $(".js-about-link").removeClass("menu-focus");
-    $(".js-contact-link").removeClass("menu-focus");
-    $(".footer").removeClass("hide-footer");
-  });
-}
+// function callToActionLink() {
+//   $(".js-main").on("click", ".js-call-to-action", event => {
+//     event.preventDefault();
+//     $(".js-projects-link").addClass("menu-focus");
+//     $(".js-about-link").removeClass("menu-focus");
+//     $(".js-contact-link").removeClass("menu-focus");
+//     $(".footer").removeClass("hide-footer");
+//   });
+// }
 
-function projectDetailsLink() {
-  $(".js-main").on("click", ".js-project-details-link", function(event) {
-    event.preventDefault();
-    $(".js-projects-link").addClass("menu-focus");
-    $(".js-about-link").removeClass("menu-focus");
-    $(".js-contact-link").removeClass("menu-focus");
-    $(".footer").removeClass("hide-footer");
-    renderQuizAppProject();
-  });
-}
+// function projectDetailsLink() {
+//   $(".js-main").on("click", ".js-project-details-link", function(event) {
+//     event.preventDefault();
+//     $(".js-projects-link").addClass("menu-focus");
+//     $(".js-about-link").removeClass("menu-focus");
+//     $(".js-contact-link").removeClass("menu-focus");
+//     $(".footer").removeClass("hide-footer");
+//     renderQuizAppProject();
+//   });
+// }
 
-callToActionLink();
-projectDetailsLink();
-quizAppFullScreenshotsLink();
+// callToActionLink();
+// projectDetailsLink();
+// quizAppFullScreenshotsLink();
